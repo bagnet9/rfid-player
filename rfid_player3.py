2a160bbd2175260b53c25afd706e17c88f17e591
sacus <75908397+Sacus1@users.noreply.github.com>
2025-10-27 21:17:41 +0100

    Play audio while downloading

--- rfid_player3.py
+++ rfid_player3.py
@@ -103,7 +103,7 @@ while continue_reading:
                         logging.info(f"🌐 Lecture du flux en ligne : {track_name}")
                         if not os.path.exists(audio_path):
                             subprocess.run(["wget", "-O", audio_path, track_name])
-                        PlayAudio(audio_path)
+                        PlayAudio(track_name)
                     else:
                         logging.warning(f"Fichier introuvable : {audio_path}")
                 else:
