e13fa43baa4cdebdca0cb3f0d25883e84463af49
sacus <75908397+Sacus1@users.noreply.github.com>
2025-11-07 22:02:22 +0100

    Browser killer

--- main_web.py
+++ main_web.py
@@ -2,26 +2,36 @@ import os
 import mmap
 from flask import Flask, render_template, request, redirect
 import json
+import threading
 app = Flask(__name__)
 app.config['MUSIC_FOLDER'] = './Music'
-@app.route('/')
-def main():
+
+def read_uuid():
     with open('shared_data.dat', 'r+b') as f:
         with mmap.mmap(f.fileno(), 1024, access=mmap.ACCESS_READ) as mm:
             mm.seek(0)
             data = mm.readline().rstrip(b'\0')
-            print("uuid :" + data.decode('utf-8'))
-            music = ""
-            musics = os.listdir(app.config['MUSIC_FOLDER'])
-            print(musics)
-            with open('UID_TO_TRACK.json', 'r') as json_file:
-                uid_to_track = json.load(json_file)
-                print(uid_to_track)
-                if data.decode('utf-8') in uid_to_track:
-                    music = uid_to_track[data.decode('utf-8')]
-                    # remove the extension
-                    music = music.split('.')[0]
-            return render_template('index.html',uuid=data.decode('utf-8'), music=music,musics=musics)
+            return data.decode('utf-8')
+
+@app.route('/check_uuid')
+def check_uuid():
+    return read_uuid()
+
+@app.route('/')
+def main():
+    with open('shared_data.dat', 'r+b') as f:
+        uuid = read_uuid()
+        music = ""
+        musics = os.listdir(app.config['MUSIC_FOLDER'])
+        print(musics)
+        with open('UID_TO_TRACK.json', 'r') as json_file:
+            uid_to_track = json.load(json_file)
+            print(uid_to_track)
+            if uuid in uid_to_track:
+                music = uid_to_track[uuid]
+                # remove the extension
+                music = music.split('.')[0]
+            return render_template('index.html',uuid=uuid, music=music,musics=musics)
 
 @app.route('/upload', methods=['POST'])
 def upload():
